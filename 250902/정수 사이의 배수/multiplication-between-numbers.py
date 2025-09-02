9ebfb811838cdd3c4a654efadec435322765bc04
Jeongin Im <131949826+harperim@users.noreply.github.com>
2025-09-02 18:47:09 +0900

    Title: 정수 사이의 배수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250902/정수 사이의 배수/multiplication-between-numbers.py
+++ 250902/정수 사이의 배수/multiplication-between-numbers.py
@@ -1,13 +1,10 @@
 a, b = map(int, input().split())
 
-num5 = []
-num7 = []
+num_li = []
 for i in range(a, b+1):
-    if i % 5 == 0:
-        num5.append(i)
-    if i % 7 == 0:
-        num7.append(i)
-sum_val = 0
-sum_val = sum(num5) + sum(num7)
-average_val = round(sum_val / (len(num5) + len(num7)), 1)
+    if i % 5 == 0 or i % 7 == 0:
+        num_li.append(i)
+   
+sum_val = sum(num_li)
+average_val = round(sum_val / (len(num_li)), 1)
 print(sum_val, average_val)
