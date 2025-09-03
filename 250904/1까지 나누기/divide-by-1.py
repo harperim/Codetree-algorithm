65964f58db7713bd3456dc5bece6d33ac3927849
Jeongin Im <131949826+harperim@users.noreply.github.com>
2025-09-04 01:37:57 +0900

    Title: 1까지 나누기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/1까지 나누기/divide-by-1.py
+++ 250904/1까지 나누기/divide-by-1.py
@@ -2,10 +2,8 @@ n = int(input())
 
 cnt = 0
 for i in range(1, n+1):
-    if n > i:
-        n //= i
-        cnt += 1
-    else:
-        cnt += 1
+    n //= i
+    cnt += 1
+    if n <= 1:
         print(cnt)
         break
